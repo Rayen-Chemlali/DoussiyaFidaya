cc0c9bd7fc6f443139e10c105242597861ba10c2
Rayen-Chemlali <rayen.chemlali@insat.ucar.tn>
2025-05-02 10:42:49 +0100

    xray and ctscan fixed

--- src/xray-results/xray-results.resolver.ts
+++ src/xray-results/xray-results.resolver.ts
@@ -1,34 +1,34 @@
 import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
 import { XrayResultsService } from './xray-results.service';
-import { XrayResult } from './entities/xray-result.entity';
+import { XRayResult } from './entities/xray-result.entity';
 import { CreateXrayResultInput } from './dto/create-xray-result.input';
 import { UpdateXrayResultInput } from './dto/update-xray-result.input';
 
-@Resolver(() => XrayResult)
+@Resolver(() => XRayResult)
 export class XrayResultsResolver {
   constructor(private readonly xrayResultsService: XrayResultsService) {}
 
-  @Mutation(() => XrayResult)
+  @Mutation(() => XRayResult)
   createXrayResult(@Args('createXrayResultInput') createXrayResultInput: CreateXrayResultInput) {
     return this.xrayResultsService.create(createXrayResultInput);
   }
 
-  @Query(() => [XrayResult], { name: 'xrayResults' })
+  @Query(() => [XRayResult], { name: 'xrayResults' })
   findAll() {
     return this.xrayResultsService.findAll();
   }
 
-  @Query(() => XrayResult, { name: 'xrayResult' })
+  @Query(() => XRayResult, { name: 'xrayResult' })
   findOne(@Args('id', { type: () => Int }) id: number) {
     return this.xrayResultsService.findOne(id);
   }
 
-  @Mutation(() => XrayResult)
+  @Mutation(() => XRayResult)
   updateXrayResult(@Args('updateXrayResultInput') updateXrayResultInput: UpdateXrayResultInput) {
     return this.xrayResultsService.update(updateXrayResultInput.id, updateXrayResultInput);
   }
 
-  @Mutation(() => XrayResult)
+  @Mutation(() => XRayResult)
   removeXrayResult(@Args('id', { type: () => Int }) id: number) {
     return this.xrayResultsService.remove(id);
   }
