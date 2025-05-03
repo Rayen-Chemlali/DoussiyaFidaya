c39c0e5f140089e7cb80ee123163bab6ab846a43
Khalil Ghimaji <khalilghimaji@gmail.com>
2025-05-03 01:53:56 +0100

    npm start works without errors after merge

--- src/common/generic.resolver.ts
+++ src/common/generic.resolver.ts
@@ -21,32 +21,32 @@ export function createResolver<TEntity, TCreateInput, TUpdateInput>(
 
     @Query(() => [entity], { name: `findAll${entity.name}s` })
     async findAll(): Promise<TEntity[]> {
-      return this.service.findAll();
+      return this.service.genericFindAll();
     }
 
     @Query(() => entity, { name: `find${entity.name}ById` })
     async findById(@Args('id') id: number): Promise<TEntity> {
-      return this.service.findOne(id, []);
+      return this.service.genericFindOne(id, []);
     }
 
-    // @Mutation(() => entity, { name: `create${entity.name}` })
-    // async create(@Args('input') input: TCreateInput): Promise<TEntity> {
-    //   return this.service.create(input);
-    // }
-    //
+    @Mutation(() => entity, { name: `create${entity.name}` })
+    async create(@Args('input',{type:()=>createInput}) input: TCreateInput): Promise<TEntity> {
+      return this.service.genericCreate(input);
+    }
+
     @Mutation(() => entity, { name: `update${entity.name}` })
     async update(@Args('id') id: number, @Args('input',{type:()=>updateInput}) input: TUpdateInput): Promise<TEntity> {
-      return this.service.update(id, input);
+      return this.service.genericUpdate(id, input);
     }
 
     @Mutation(() => entity, { name: `delete${entity.name}` })
     async delete(@Args('id') id: number): Promise<TEntity> {
-      return this.service.remove(id);
+      return this.service.genericRemove(id);
     }
 
     @Mutation(() => [entity], { name: `find${entity.name}sByIds` })
     async findByIds(@Args('ids', { type: () => [Number] }) ids: number[]): Promise<TEntity[]> {
-      return this.service.findByIds(ids);
+      return this.service.genericFindByIds(ids);
     }
   }
 
--- src/consultations/dto/create-consultation.input.ts
+++ src/consultations/dto/create-consultation.input.ts
@@ -3,5 +3,18 @@ import { Consultation } from '../entities/consultation.entity';
 
 @InputType()
 export class CreateConsultationInput extends OmitType(Consultation,['id','patient','doctor','institut_medical','prescription','lab_requests']) {
+  @Field(() => String, { description: 'ID of the patient' })
+  patient_id: string;
 
+  @Field(() => String, { description: 'ID of the doctor' })
+  doctor_id: string;
+
+  @Field(() => String, { description: 'ID of the medical institute' })
+  institut_medical_id: string;
+
+  @Field(() => String, { description: 'ID of the prescription' })
+  prescription_id: string;
+
+  @Field(() => [String], { description: 'IDs of the lab requests' })
+  lab_request_ids: string[];
 }
